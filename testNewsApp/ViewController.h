f57a2655a79b1e32c838b6052a0eb5cfb3eac925
Evgeny Patrikeev <aawee@yandex.ru>
2018-07-02 15:38:00 +0300

    Initial Commit

---
+++ testNewsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testNewsApp
+//
+//  Created by Evgeny Patrikeev on 02.07.2018.
+//  Copyright © 2018 Evgeny Patrikeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
